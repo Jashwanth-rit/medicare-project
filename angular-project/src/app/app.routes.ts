1931eaa1d98ed4e13ee37c3944de7b9cda0681aa
Jashwanth-rit <158207370+Jashwanth-rit@users.noreply.github.com>
2024-12-20 18:41:29 +0530

    add wildcard route to redirect unmatched paths to home

--- angular-project/src/app/app.routes.ts
+++ angular-project/src/app/app.routes.ts
@@ -85,6 +85,7 @@ export const routes: Routes = [
         path:'',
         component:MainHomeComponent
     },
+    { path: '**', redirectTo: '/' },
     {
         path:'seller',
         component:SellerComponent
